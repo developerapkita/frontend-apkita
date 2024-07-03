4ec8e20ab4ae98e8b3ad565c162a6acb64d84290
rifdoalam <rifdoalam@gmail.com>
2024-07-04 02:05:10 +0700

    feat(apptsx) : add login page

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Fragments/layouts/Layout";
 import Dashboard from "./pages/dashboard";
-
+import Login from "./pages/login";
+import Anggota from "./pages/anggota";
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" Component={Login} />
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
+          <Route path="/anggota" element={<Anggota />} />
         </Route>
       </Routes>
     </BrowserRouter>
